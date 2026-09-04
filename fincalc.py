0ddfac5721d74e86ba8129796b1228c6f572b2e3
bretazs <12302848@aluno.cotemig.com.br>
2026-09-04 22:52:51 +0000

    feature: calculoirrf

--- fincalc.py
+++ fincalc.py
@@ -57,13 +57,20 @@ if __name__ == "__main__":
     montante_comp = calcular_juros_compostos(1000.0, 5.0, 2)
     print(f"Juros Compostos: R$ {montante_comp:.2f}")
 
-def calcular_irrf(salario_bruto: float) -> float:
+
+def calcular_irrf(
+    salario_bruto: float
+) -> float:
     """Calcula a alíquota simplificada de Imposto de Renda Retido na Fonte."""
+
     if salario_bruto <= 2259.20:
         return 0.0
+
     elif salario_bruto <= 2826.65:
         return (salario_bruto * 0.075) - 169.44
+
     elif salario_bruto <= 3751.05:
         return (salario_bruto * 0.15) - 381.44
+
     else:
         return (salario_bruto * 0.225) - 662.77
